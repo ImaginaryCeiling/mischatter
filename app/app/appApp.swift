71c89e9d6ef7b06f150baccc72666d912c3f4bb5
Shubham Patil <shubhampatilsd@gmail.com>
2025-08-07 01:59:27 -0700

    kabhi din door dhal jaye

---
+++ app/app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Shubham Patil on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
